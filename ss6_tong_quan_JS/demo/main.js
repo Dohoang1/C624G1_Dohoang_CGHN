03db5e904823a9ed1bbeb420af5119d1b46916b9
Dohoang1 <ihngno1@gmail.com>
2024-07-06 10:41:01 +0700

    Hoang up demo JS

---
+++ ss6_tong_quan_JS/demo/main.js
@@ -0,0 +1,30 @@
+//khai bao bien
+/*
+noi dung o day la comment
+ */
+// var fullName = 'Do Hoang';
+// var age = 26;
+//
+// // goi ham thong bao
+// alert(fullName);
+// alert(age);
+//
+// //khai bao console
+// console.log(fullName)
+//
+// // goi ham xac nhan
+// confirm('Xac nhan ban du tuoi!');
+//
+// //goi ham xac nhan va nhap them thong tin
+// prompt('ban bao nhieu tuoi?');
+//
+// //set timeout
+// setTimeout(function () {
+//     alert('alo')
+// }, 1000);
+//
+//
+// //set interval
+// setInterval(function (){
+//     alert('nghe ko?')
+// },2000);
